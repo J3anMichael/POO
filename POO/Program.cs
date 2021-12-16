347b34d677927b0353434c41df0dfdd0b7ef6d99
Jean Michael <jeanmmclaudino@gmail.com>
2021-12-16 20:33:08 -0300

    Classes

--- POO/Program.cs
+++ POO/Program.cs
@@ -1,44 +1,87 @@
 ﻿using System;
+using POO;
 
 namespace Poo
 {
-     class Program
+    class Program
     {
         static void Main(string[] args)
         {
 
             // EXEMPLO SEM POO
 
-            double xA, xB, xC, yA, yB, yC;
+            //double xA, xB, xC, yA, yB, yC;
+
+            //Console.WriteLine("Entre com as medidas do triangulo X: ");
+
+            //xA = double.Parse(Console.ReadLine());
+            //xB = double.Parse(Console.ReadLine());
+            //xC = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Entre com as mendidas do triangulo Y:");
+
+            //yA = double.Parse(Console.ReadLine());
+            //yB = double.Parse(Console.ReadLine());
+            //yC = double.Parse(Console.ReadLine());
+
+            //double p = (xA + xB + xC) / 2.0;
+            //double areaX = Math.Sqrt(p * (p - xA) * (p - xB) * (p - xC));
+
+            //p = (yA + yB + yC) / 2.0;
+            //double areaY = Math.Sqrt(p * (p - yA) * (p - yB) * (p - yC));
+
+            //Console.WriteLine("Area de  X " + areaX.ToString("F4"));
+            //Console.WriteLine("Area de  Y " + areaY.ToString("F4"));
+
+            //if ( areaX > areaY)
+            //{
+            //    Console.WriteLine("Maior area: X");
+            //}
+            //else
+            //{s
+            //    Console.WriteLine("Maior area: Y");
+
+
+            //}
+
+
+            // EXEMPLO COM CLASSE
+
+            Triangulo x, y;
+
+            x = new Triangulo();
+            y = new Triangulo();
 
             Console.WriteLine("Entre com as medidas do triangulo X: ");
 
-            xA = double.Parse(Console.ReadLine());
-            xB = double.Parse(Console.ReadLine());
-            xC = double.Parse(Console.ReadLine());
+            x.A = double.Parse(Console.ReadLine());
+            x.B = double.Parse(Console.ReadLine());
+            x.C = double.Parse(Console.ReadLine());
 
             Console.WriteLine("Entre com as mendidas do triangulo Y:");
 
-            yA = double.Parse(Console.ReadLine());
-            yB = double.Parse(Console.ReadLine());
-            yC = double.Parse(Console.ReadLine());
+            y.A = double.Parse(Console.ReadLine());
+            y.B = double.Parse(Console.ReadLine());
+            y.C = double.Parse(Console.ReadLine());
 
-            double p = (xA + xB + xC) / 2.0;
-            double areaX = Math.Sqrt(p * (p - xA) * (p - xB) * (p - xC));
+            double p = (x.A + x.B + x.C) / 2.0;
+            double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
 
-            p = (yA + yB + yC) / 2.0;
-            double areaY = Math.Sqrt(p * (p - yA) * (p - yB) * (p - yC));
+            p = (y.A + y.B + y.C) / 2.0;
+            double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
 
             Console.WriteLine("Area de  X " + areaX.ToString("F4"));
             Console.WriteLine("Area de  Y " + areaY.ToString("F4"));
 
-            if ( areaX > areaY)
+            if (areaX > areaY)
             {
                 Console.WriteLine("Maior area: X");
             }
             else
             {
+
                 Console.WriteLine("Maior area: Y");
+
             }
         }
     }
